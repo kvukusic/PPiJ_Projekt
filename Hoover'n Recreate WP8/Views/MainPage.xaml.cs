fb06383c92c0ea90477dc5e09473c552d56317cb
kvukusic1 <kristian.vukusic@gmail.com>
2014-05-11 09:45:03 +0200

    Added WP8 Project

---
+++ Hoover'n Recreate WP8/Views/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Hoover__n_Recreate.Services;
+using Microsoft.Devices;
+using Microsoft.Devices.Sensors;
+using Microsoft.Phone.Controls;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Matrix = Microsoft.Xna.Framework.Matrix;
+
+#endregion
+
+namespace Hoover__n_Recreate.Views
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TestGrid_OnTap(object sender, GestureEventArgs e)
+        {
+            //Services.NavigationService.Instance.Navigate(PageNames.TestPageViewName);
+        }
+    }
+}
---
+++ Hoover'n Recreate WP8/Weather/City.cs
@@ -0,0 +1,93 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace Hoover__n_Recreate.Weather
+{
+    public class City : INotifyPropertyChanged
+    {
+        private string _CityName;
+        private string _Latitude;
+        private string _Longitude;
+
+        public string CityName
+        {
+            get
+            {
+                return _CityName;
+            }
+            set
+            {
+                if (value != _CityName)
+                {
+                    _CityName = value;
+                    NotifyPropertyChanged("CityName");
+                }
+            }
+        }
+
+        public string Latitude
+        {
+            get
+            {
+                return _Latitude;
+            }
+            set
+            {
+                if (value != _Latitude)
+                {
+                    _Latitude = value;
+                    NotifyPropertyChanged("Latitude");
+                }
+            }
+        }
+
+        public string Longitude
+        {
+            get
+            {
+                return _Longitude;
+            }
+            set
+            {
+                if (value != _Longitude)
+                {
+                    _Longitude = value;
+                    NotifyPropertyChanged("Longitude");
+                }
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        /// <summary>
+        /// Constructor with full city information
+        /// </summary>
+        public City(string cityName, string latitude, string longitude)
+        {
+            CityName = cityName;
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        /// <summary>
+        /// Raise the PropertyChanged event and pass along the property that changed
+        /// </summary>
+        private void NotifyPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
