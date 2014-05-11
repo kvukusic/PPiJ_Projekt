d7681cf355838303494a4a1f1c8fbe7d663d2551
kvukusic1 <kristian.vukusic@gmail.com>
2014-05-11 12:11:49 +0200

    Added functional map

--- Hoover/Views/MainPage.xaml.cs
+++ Hoover/Views/MainPage.xaml.cs
@@ -1,20 +1,132 @@
-﻿using System;
+﻿#region
+
+using System;
 using System.Collections.Generic;
+using System.Device.Location;
 using System.Linq;
 using System.Net;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GART.Controls;
 using Microsoft.Phone.Controls;
+using Microsoft.Phone.Maps.Controls;
+using Microsoft.Phone.Maps.Services;
 using Microsoft.Phone.Shell;
+
+#endregion
 
 namespace Hoover.Views
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        private GeoCoordinate _myLocation;
+        private GeoCoordinate _destination;
+        private MapRoute _mapRoute;
+
+        private List<GeoCoordinate> _currentWaypoints;
+
         public MainPage()
         {
             InitializeComponent();
+
+            InitMainPage();
+        }
+
+        private void InitMainPage()
+        {
+            
+        }
+
+        /// <summary>
+        /// Called when a page becomes the active page in a frame.
+        /// </summary>
+        /// <param name="e">An object that contains the event data.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            ArDisplay.StartServices();
+
+            this._currentWaypoints = new List<GeoCoordinate>();
+
+            this._myLocation = ArDisplay.Location;
+            this._currentWaypoints.Add(_myLocation);
+        }
+
+        /// <summary>
+        /// Called when a page is no longer the active page in a frame.
+        /// </summary>
+        /// <param name="e">An object that contains the event data.</param>
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+
+            ArDisplay.StopServices();
+        }
+
+        private void OverheadMap_OnTap(object sender, GestureEventArgs e)
+        {
+            Map mapControl = this.OverheadMap.Map;
+            if (mapControl != null)
+            {
+                _destination = mapControl.ConvertViewportPointToGeoCoordinate(e.GetPosition(mapControl));
+                // Add the current destination to the waypoints list
+
+                mapControl.Layers.Add(new MapLayer() {
+				    new MapOverlay()
+				    {
+				        GeoCoordinate = _destination,
+					    PositionOrigin = new Point(0.5, 0.5),
+					    Content = CreateIndicator()
+				    }});
+                _currentWaypoints.Add(_destination);
+
+                RouteQuery query = new RouteQuery()
+                {
+                    TravelMode = TravelMode.Walking,
+                    Waypoints = _currentWaypoints
+                };
+
+                query.QueryCompleted += routeQuery_QueryCompleted;
+                query.QueryAsync();
+            }
+        }
+
+        /// <summary>
+        /// Creates the indicator on a map.
+        /// </summary>
+        /// <returns></returns>
+        private UIElement CreateIndicator()
+        {
+            return new Ellipse()
+            {
+                Fill = new SolidColorBrush(Colors.Red),
+                Width = 15,
+                Height = 15
+            };
+        }
+
+        private void routeQuery_QueryCompleted(object sender, QueryCompletedEventArgs<Route> e)
+        {
+            try
+            {
+                Map mapControl = this.OverheadMap.Map;
+                if (mapControl != null)
+                {
+                    if (_mapRoute != null) mapControl.RemoveRoute(_mapRoute);
+                    _mapRoute = new MapRoute(e.Result);
+                    mapControl.AddRoute(_mapRoute);
+                    //routeInfo.Text = "Distance: " + e.Result.LengthInMeters + "\nTime: " + e.Result.EstimatedDuration;
+                    //routeInfo.Visibility = System.Windows.Visibility.Visible;
+                }
+            }
+            catch (Exception)
+            {
+            }
         }
     }
 }
