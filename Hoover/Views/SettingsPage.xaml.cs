556a04a9fc4a675e0b5abd21f6bcf8b560aac819
mskovacic <mskovacic10@gmail.com>
2014-05-17 21:12:27 +0200

    commit

--- Hoover/Views/SettingsPage.xaml.cs
+++ Hoover/Views/SettingsPage.xaml.cs
@@ -1,6 +1,4 @@
-﻿#region
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -10,8 +8,7 @@ using System.Windows.Navigation;
 using Hoover.Settings;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
-
-#endregion
+using Microsoft.Phone.Tasks;
 
 namespace Hoover.Views
 {
@@ -31,7 +28,12 @@ namespace Hoover.Views
 
         private void LoginButton_Click(object sender, RoutedEventArgs e)
         {
+            ShareLinkTask shareLinkTask = new ShareLinkTask();
+            shareLinkTask.Title = "Code Samples";
+            shareLinkTask.LinkUri = new Uri("http://code.msdn.com/wpapps", UriKind.Absolute);
+            shareLinkTask.Message = "Here are some great code samples for Windows Phone.";
 
+            shareLinkTask.Show();
         }
 
         private void FacebookToogleSwitch_Unchecked(object sender, RoutedEventArgs e)
