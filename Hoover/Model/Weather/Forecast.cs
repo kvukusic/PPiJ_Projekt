f4eb170926776419de8041c86d2761940d17d5f6
kvukusic1 <kristian.vukusic@gmail.com>
2014-05-17 20:07:03 +0200

    Added Weather Forecast model

---
+++ Hoover/Model/Weather/Forecast.cs
@@ -0,0 +1,25 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace Hoover.Model.Weather
+{
+    public class Forecast
+    {
+        /// <summary>
+        /// The city for which the forecast is requested.
+        /// </summary>
+        public City City { get; set; }
+
+        /// <summary>
+        /// The forecast items.
+        /// </summary>
+        public List<ForecastItem> ForecastItems { get; set; }
+    }
+}
