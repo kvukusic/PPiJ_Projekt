7d6e8c364f108fff4ddd682abe796dbf969659a7
kvukusic1 <kristian.vukusic@gmail.com>
2014-05-20 19:42:10 +0200

    Motion Constants

---
+++ Hoover/Helpers/MotionConstants.cs
@@ -0,0 +1,27 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Devices.Sensors;
+
+#endregion
+
+namespace Hoover.Helpers
+{
+    public class MotionConstants
+    {
+        /// <summary>
+        /// Will be true if Motion API is enabled on this device.
+        /// </summary>
+        public bool IsMotionEnabled
+        {
+            get
+            {
+                return Motion.IsSupported;
+            }
+        }
+    }
+}
