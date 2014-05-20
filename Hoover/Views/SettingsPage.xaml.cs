4368915514df61cc88cbe202df3b4feac7ea759f
mskovacic <mskovacic10@gmail.com>
2014-05-20 19:32:25 +0200

    hmmm

--- Hoover/Views/SettingsPage.xaml.cs
+++ Hoover/Views/SettingsPage.xaml.cs
@@ -22,24 +22,6 @@ namespace Hoover.Views
             this.DataContext = ApplicationSettings.Instance;
         }
 
-        private void FacebookToogleSwitch_Checked(object sender, RoutedEventArgs e)
-        {
-            FacebookLogin.Visibility = System.Windows.Visibility.Visible;
-        }
-
-        private void LoginButton_Click(object sender, RoutedEventArgs e)
-        {
-            ShareLinkTask shareLinkTask = new ShareLinkTask();
-            shareLinkTask.Title = "Code Samples";
-            shareLinkTask.LinkUri = new Uri("http://code.msdn.com/wpapps", UriKind.Absolute);
-            shareLinkTask.Message = "Here are some great code samples for Windows Phone.";
-
-            shareLinkTask.Show();
-        }
-
-        private void FacebookToogleSwitch_Unchecked(object sender, RoutedEventArgs e)
-        {
-            //FacebookLogin.Visibility = System.Windows.Visibility.Collapsed;
-        }
+        
     }
 }
