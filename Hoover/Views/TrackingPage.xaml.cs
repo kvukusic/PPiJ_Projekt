bead4abc99494af308b060883f5246811378c077
pcurko10 <petar.curkovic@windowslive.com>
2014-05-20 18:28:36 +0200

    AddedMotion

--- Hoover/Views/TrackingPage.xaml.cs
+++ Hoover/Views/TrackingPage.xaml.cs
@@ -34,6 +34,7 @@ using Hoover.Common;
 using System.Windows.Threading;
 using Microsoft.Devices.Sensors;
 using Hoover.Services;
+using Microsoft.Xna.Framework;
 
 #endregion
 
@@ -60,6 +61,8 @@ namespace Hoover.Views
 		private long _previousPositionChangeTick;
 		private int _activeCheckpoint;
 		private Model.HistoryItem _currentRoute;
+
+		private Motion _motion;
 
 		#endregion
 
@@ -203,6 +206,18 @@ namespace Hoover.Views
 
 			StartRoute();
 			_watcher.Start();
+
+			_motion = new Motion();
+			_motion.CurrentValueChanged += _motion_CurrentValueChanged;
+			_motion.Start();
+		}
+
+		void _motion_CurrentValueChanged(object sender, SensorReadingEventArgs<MotionReading> e)
+		{
+			Dispatcher.BeginInvoke(delegate
+			{
+				this.yaw.Text = MathHelper.ToDegrees(e.SensorReading.Attitude.Yaw).ToString("0");
+			});
 		}
 
 		private void StopButton_Tap(object sender, System.Windows.Input.GestureEventArgs e)
@@ -271,7 +286,7 @@ namespace Hoover.Views
 				this.totalDistanceRun.Text = Helpers.Extensions.Length(_distance);
 				this.totalDistance.Text = Helpers.Extensions.Speed(_distance / (DateTime.Now - _startTime).TotalSeconds);
 
-				if (_waypoints.Count > _activeCheckpoint && coord.GetDistanceTo(_waypoints[_activeCheckpoint]) < 10)
+				if (_waypoints.Count > _activeCheckpoint && coord.GetDistanceTo(_waypoints[_activeCheckpoint]) < 30)
 				{
 					SetNewCheckpoint();
 				}
@@ -369,7 +384,7 @@ namespace Hoover.Views
 				new MapOverlay() {
 					Content = new Pushpin() { Content = content },
 					GeoCoordinate = location,
-					PositionOrigin = new Point(0,0)
+					PositionOrigin = new System.Windows.Point(0,0)
 				}
 			});
 		}
