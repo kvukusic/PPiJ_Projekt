8117e9ebdf26c094dbc4049dc4734fd542d9bea1
kvukusic1 <kristian.vukusic@gmail.com>
2014-05-22 19:33:04 +0200

    Popup Service

---
+++ Hoover/Helpers/AnimationsFactory.cs
@@ -0,0 +1,95 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Animation;
+using Telerik.Windows.Controls;
+
+#endregion
+
+namespace Hoover.Helpers
+{
+    /// <summary>
+    /// Helper for retreiving animations.
+    /// </summary>
+    public static class AnimationsFactory
+    {
+        /// <summary>
+        /// Returns the <see cref="RadEmptyAnimation"/>.
+        /// </summary>
+        /// <returns></returns>
+        public static RadEmptyAnimation RadEmptyAnimation()
+        {
+            return new RadEmptyAnimation();
+        }
+
+        /// <summary>
+        /// Default <see cref="RadMoveAndFadeAnimation"/>.
+        /// </summary>
+        /// <returns></returns>
+        public static RadMoveAndFadeAnimation RadMoveAndFadeAnimationDefault()
+        {
+            var moveAndFade = new RadMoveAndFadeAnimation();
+            moveAndFade.MoveAnimation.StartPoint = new Point(0, -90);
+            moveAndFade.MoveAnimation.EndPoint = new Point(0, 0);
+            moveAndFade.FadeAnimation.StartOpacity = 0;
+            moveAndFade.FadeAnimation.EndOpacity = 1;
+            moveAndFade.Easing = new CubicEase();
+
+            return moveAndFade;
+        }
+
+        /// <summary>
+        /// Default <see cref="RadMoveAndFadeAnimation"/>.
+        /// </summary>
+        /// <returns></returns>
+        public static RadMoveAnimation RadMoveAnimationDefault()
+        {
+            var move = new RadMoveAnimation();
+            move.StartPoint = new Point(0, -90);
+            move.EndPoint = new Point(0, 0);
+            move.Easing = new CubicEase();
+
+            return move;
+        }
+
+        /// <summary>
+        /// Default <see cref="RadMoveAnimation"/>
+        /// </summary>
+        /// <returns></returns>
+        public static RadMoveAnimation RadMoveAnimationRemoveDefault()
+        {
+            var moveAnimation = new RadMoveAnimation();
+            moveAnimation.StartPoint = new Point(0, 0);
+            moveAnimation.EndPoint = new Point(-500, 0);
+            moveAnimation.Duration = TimeSpan.FromMilliseconds(300);
+            moveAnimation.Easing = new CubicEase() { EasingMode = EasingMode.EaseOut };
+
+            return moveAnimation;
+        }
+
+        /// <summary>
+        /// Default <see cref="RadPlaneProjectionAnimation"/>.
+        /// </summary>
+        /// <returns></returns>
+        public static RadPlaneProjectionAnimation RadPlaneProjectionAnimation()
+        {
+            return new RadPlaneProjectionAnimation
+            {
+                CenterY = 0.5,
+                CenterX = 0,
+                StartAngleX = -90,
+                EndAngleX = 0,
+                Axes = PerspectiveAnimationAxis.X,
+                Easing = new CubicEase()
+                {
+                    EasingMode = EasingMode.EaseOut
+                }
+            };
+        }
+    }
+}
---
+++ Hoover/Services/PopupService.cs
@@ -0,0 +1,153 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Hoover.Helpers;
+using Hoover.Model;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Telerik.Windows.Controls;
+
+#endregion
+
+namespace Hoover.Services
+{
+    /// <summary>
+    /// This singleton contains the logic for displaying specialized popups.
+    /// </summary>
+    public sealed class PopupService
+    {
+
+        #region Singleton
+
+        /// <summary>
+        /// Private constructor.
+        /// </summary>
+        private PopupService()
+        {
+
+        }
+
+        // Explicit static constructor to tell C# compiler
+        // not to mark type as beforefieldinit
+        static PopupService()
+        {
+        }
+
+        private static readonly PopupService _instance = new PopupService();
+
+        public static PopupService Instance
+        {
+            get { return _instance; }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Displays the weather popup. Returns a <see cref="Task"/> which will be completed after the popup is closed.
+        /// </summary>
+        public async Task ShowWeatherPopup()
+        {
+            Deployment.Current.Dispatcher.BeginInvoke(async () =>
+            {
+                //await ShowPopup(new PlayerDetailsView(playerId));
+            });
+        }
+
+        /// <summary>
+        /// Displays popup for session completion. Returns a <see cref="Task"/> which will be completed after the popup is closed.
+        /// </summary>
+        public async Task ShowSessionCompletedPopup(HistoryItem historyItem)
+        {
+            Deployment.Current.Dispatcher.BeginInvoke(async () =>
+            {
+                //await ShowPopup(new PlayerDetailsView(playerId));
+            });
+        }
+
+        /// <summary>
+        /// Shows the popup
+        /// </summary>
+        /// <param name="control">The view which will be displayed in the popup.</param>
+        /// <param name="closingSource">When not null, this source will be set to close the window.</param>
+        /// <returns></returns>
+        private async Task ShowPopup(UserControl control, TaskCompletionSource<bool> closingSource = null)
+        {
+            TaskCompletionSource<bool> waitForClose = closingSource;
+
+            RadModalWindow window = new RadModalWindow();
+            window.Background = new SolidColorBrush(Colors.Black)
+            {
+                Opacity = 0.5
+            };
+            window.Placement = PlacementMode.CenterCenter;
+            window.IsClosedOnOutsideTap = true;
+            window.HorizontalAlignment = HorizontalAlignment.Stretch;
+            window.VerticalAlignment = VerticalAlignment.Center;
+
+            window.Content = control;
+
+            window.IsAnimationEnabled = true;
+
+            window.OpenAnimation = AnimationsFactory.RadMoveAnimationDefault();
+            window.CloseAnimation = AnimationsFactory.RadMoveAnimationRemoveDefault();
+
+            // Hide app bar before opening
+            IApplicationBar appBar = null;
+            var currentFrame = Application.Current.RootVisual as PhoneApplicationFrame;
+            if (currentFrame != null)
+            {
+                var currentPage = currentFrame.Content as PhoneApplicationPage;
+                if (currentPage != null)
+                {
+                    appBar = currentPage.ApplicationBar;
+                }
+            }
+            bool beforePopupAppBarState = true;
+            if (appBar != null)
+            {
+                beforePopupAppBarState = appBar.IsVisible;
+                appBar.IsVisible = false;
+            }
+
+            // Show app bar on closing
+            if (window.CloseAnimation == null)
+            {
+                window.WindowClosed += (sender, args) =>
+                {
+                    if (appBar != null)
+                    {
+                        appBar.IsVisible = beforePopupAppBarState;
+                    }
+                    if (waitForClose != null) waitForClose.TrySetResult(true);
+                };
+            }
+            else
+            {
+                window.CloseAnimation.Ended += (sender, args) =>
+                {
+                    if (appBar != null)
+                    {
+                        appBar.IsVisible = beforePopupAppBarState;
+                    }
+                    if (waitForClose != null) waitForClose.TrySetResult(true);
+                };
+            }
+
+            window.IsOpen = true;
+
+            if (waitForClose != null)
+            {
+                await waitForClose.Task;
+                window.IsOpen = false;
+            }
+        }
+
+    }
+}
